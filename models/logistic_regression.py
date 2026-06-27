1d5f8cc19eddcbf25cac7aaca7e631832133f8cd
bhavyalboddu <bhavyalboddu@gmail.com>
2026-06-27 00:21:59 -0400

    added logistic regression model

---
+++ models/logistic_regression.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+def train_model(train_prepared, train_labels):
+    model = LogisticRegression(max_iter=10000, random_state=0)
+    model.fit(train_prepared, train_labels)
+    return model
+
+def evaluate_model(model, test_prepared, test_labels):
+    classifier_predictions = model.predict(test_prepared)
+    accuracy = accuracy_score(test_labels, classifier_predictions)*100
+    return accuracy, classifier_predictions
+
+def generate_classification_report(classifier_predictions, test_labels):
+    return classification_report(test_labels, classifier_predictions)
