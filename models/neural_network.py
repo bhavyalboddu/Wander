b26118aa3af57f36362a83e83372bd330bfd6181
bhavyalboddu <bhavyalboddu@gmail.com>
2026-06-29 18:57:08 -0400

    added randomized_cv search to scikit learn neural network model

--- models/neural_network.py
+++ models/neural_network.py
@@ -1,5 +1,5 @@
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import accuracy_score, classification_report
 
 def train_model(train_prepared, train_labels):
@@ -15,7 +15,7 @@ def evaluate_model(model, test_prepared, test_labels):
 def generate_classification_report(classifier_predictions, test_labels):
     return classification_report(test_labels, classifier_predictions)
 
-def tune_model(model, train_prepared, train_labels, test_prepared, test_labels, cv=5):
+def tune_model_grid(model, train_prepared, train_labels, test_prepared, test_labels, cv=5):
 
     param_grid = {
         "hidden_layer_sizes": [(32,), (32, 16), (64, 32), (128, 64, 32)],  # Different layer structures
@@ -36,3 +36,31 @@ def tune_model(model, train_prepared, train_labels, test_prepared, test_labels, 
     test_acc = accuracy_score(test_labels, test_predictions) * 100
  
     return grid_search, best_model, test_acc
+
+def tune_model_randomized(train_prepared, train_labels, test_prepared, test_labels, cv=5):
+    
+    mlp = MLPClassifier(max_iter=1000, early_stopping=True, random_state=41)
+
+    param_dist = {
+        "hidden_layer_sizes": [(32,), (32, 16), (64, 32), (128, 64, 32)],
+        "activation": ["relu", "tanh"],
+        "solver": ["adam"],
+        "alpha": [0.0001, 0.001, 0.01],
+        "learning_rate": ["constant", "adaptive"],
+        "batch_size": [16, 32]
+    }
+
+    random_search = RandomizedSearchCV(mlp, param_distributions=param_dist,
+                                    n_iter=10, cv=3, scoring="roc_auc",
+                                    n_jobs=-1, verbose=2, random_state=42)
+
+    random_search.fit(train_prepared, train_labels)
+
+    print(f"Best Parameters: {random_search.best_params_}")
+    print("Best Score:", random_search.best_score_)
+
+    best_model = random_search.best_estimator_
+    test_predictions = best_model.predict(test_prepared)
+    test_acc = accuracy_score(test_labels, test_predictions) * 100
+
+    return random_search, best_model, test_acc
